11db363d9511d4dfd1f1e0d80e2f84d377704428
Emil Mikulic <emikulic@gmail.com>
2019-09-25 23:24:40 +1000

    Add argparse.

--- convert.py
+++ convert.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+import argparse
+
+def main():
+  p = argparse.ArgumentParser()
+  p.add_argument('infile')
+  p.add_argument('outfile')
+  args = p.parse_args()
+
 f = open('tani.txt')
 d = f.readlines()
 f.close()
@@ -12,5 +20,8 @@ h = h.split('00004020')
 
 for l in h:
   print(l)
+
+if __name__ == '__main__':
+  main()
 
 # vim:set sw=2 ts=2 sts=2 et tw=80:
