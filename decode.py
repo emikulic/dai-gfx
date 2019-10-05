3c722e2d7a94cb3d64e4b5901e5c35ab6a38b011
Emil Mikulic <emikulic@gmail.com>
2019-10-05 19:04:36 +1000

    Decoder: factor out line length calculation.

--- decode.py
+++ decode.py
@@ -3,6 +3,26 @@ import argparse
 import numpy as np
 from PIL import Image  # pip3 install pillow
 
+def cut(b, l):
+  """Cut the last `l` bytes from `b`, returns the left and right parts."""
+  assert len(b) >= l, (len(b), l)
+  return b[:-l], b[-l:]
+
+assert cut(b'hello', 3) == (b'he', b'llo')
+
+def get_line_len(not_unit_color, disp, res):
+  if not not_unit_color:
+    return 2  # FIXME: Why?
+  elif disp == 0 and res == 2:
+    # 2 bytes = 8 cols
+    return 352 // 8 * 2
+  elif disp == 1 and res == 3:
+    # 66 text cols * 2 bytes each
+    return 66 * 2
+  print('Unknown mode, trailing data is:')
+  print(data[-70:].hex())
+  return None
+
 def main():
   p = argparse.ArgumentParser()
   p.add_argument('infile')
@@ -33,53 +53,46 @@ def main():
   line_num = 1
   while data:
     # Control word, high address byte (mode byte) (manual section 3.2.1)
-    mode = data[-1]
+    data, mode = cut(data, 1)
+    mode = mode[0]
     # Bits:
     # 7, 6 - display mode control
     # 5, 4 - resolution control
     # 3, 2, 1, 0 - line repeat count
     # Disp:
-    # 00 -  4 color gfx
-    # 01 -  4 color chars
-    # 10 - 16 color gfx
-    # 11 - 16 color chars
+    # (0) 00 -  4 color gfx
+    # (1) 01 -  4 color chars
+    # (2) 10 - 16 color gfx
+    # (3) 11 - 16 color chars
     disp = (mode >> 6) & 3
     # Res:
-    # 00 -  88 cols
-    # 01 - 176 cols
-    # 10 - 352 cols
-    # 11 - 528 cols, text mode with 66 chars per line
+    # (0) 00 -  88 cols
+    # (1) 01 - 176 cols
+    # (2) 10 - 352 cols
+    # (3) 11 - 528 cols, text mode with 66 chars per line
     res = (mode >> 4) & 3
     line_rep = mode & 15
 
     # Low address byte (color byte)
-    color = data[-2]
+    data, color = cut(data, 1)
+    color = color[0]
     enable_change = (color >> 7) & 1
-    unit_color = 1 - ((color >> 6) & 1)  # Invert.
+    not_unit_color = (color >> 6) & 1
     color_reg = (color >> 4) & 3
     color_sel = color & 15
 
+    line_len = get_line_len(not_unit_color, disp, res)
     print(f'line {line_num}: control {data[-2:].hex()} '
         f'| color {color:02x} enable_change {enable_change} '
-        f'unit_color {unit_color} color_reg {color_reg} color_sel {color_sel} '
-        f'| mode {mode:02x} disp {disp} res {res} line_rep {line_rep}')
-    # Consume control word.
-    data = data[:-2]
-    # Determine length of line.
-    if res == 2 and disp == 0 and not unit_color:
-      # 2 bytes = 8 cols
-      ln = 352 // 8 * 2
-    elif unit_color:
-      ln = 2  # FIXME: Why?
-    else:
-      print(data[-60:].hex())
-      assert False, ('unknown mode', mode, disp, res, line_rep)
+        f'not_unit_color {not_unit_color} color_reg {color_reg} '
+        f'color_sel {color_sel} '
+        f'| mode {mode:02x} disp {disp} res {res} line_rep {line_rep} '
+        f'| len {line_len}')
+    assert line_len is not None
+
     # Consume line.
-    assert len(data) >= ln, (len(data), ln)
-    pixels = data[-ln:]
-    data = data[:-ln]
+    data, pixels = cut(data, line_len)
     pixels = pixels[::-1] # Reverse.
-    #print(f'line {line_num}: disp {disp} res {res} length {ln}')
     line_num += 1
 
     # TODO: deal with line rep
