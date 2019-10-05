821255f9d446df35bf945faa521913bbc2eba518
Emil Mikulic <emikulic@gmail.com>
2019-10-05 18:48:20 +1000

    Decoder: fixes, add better debug output.

--- decode.py
+++ decode.py
@@ -38,43 +38,51 @@ def main():
     # 7, 6 - display mode control
     # 5, 4 - resolution control
     # 3, 2, 1, 0 - line repeat count
-    line_rep = mode & 15
-    assert line_rep == 0, line_rep
-    res = (mode >> 4) & 3
-    # Res:
-    # 00 -  88 cols
-    # 01 - 176 cols
-    # 10 - 352 cols
-    # 11 - 528 cols, text mode with 66 chars per line
-    disp = (mode >> 6) & 3
     # Disp:
     # 00 -  4 color gfx
     # 01 -  4 color chars
     # 10 - 16 color gfx
     # 11 - 16 color chars
-    # --
+    disp = (mode >> 6) & 3
+    # Res:
+    # 00 -  88 cols
+    # 01 - 176 cols
+    # 10 - 352 cols
+    # 11 - 528 cols, text mode with 66 chars per line
+    res = (mode >> 4) & 3
+    line_rep = mode & 15
+
     # Low address byte (color byte)
     color = data[-2]
     enable_change = (color >> 7) & 1
-    not_unit_color = (color >> 6) & 1
+    unit_color = 1 - ((color >> 6) & 1)  # Invert.
     color_reg = (color >> 4) & 3
     color_sel = color & 15
+
+    print(f'line {line_num}: control {data[-2:].hex()} '
+        f'| color {color:02x} enable_change {enable_change} '
+        f'unit_color {unit_color} color_reg {color_reg} color_sel {color_sel} '
+        f'| mode {mode:02x} disp {disp} res {res} line_rep {line_rep}')
     # Consume control word.
     data = data[:-2]
     # Determine length of line.
-    if res == 2 and disp == 0:
+    if res == 2 and disp == 0 and not unit_color:
       # 2 bytes = 8 cols
       ln = 352 // 8 * 2
+    elif unit_color:
+      ln = 2  # FIXME: Why?
     else:
+      print(data[-60:].hex())
       assert False, ('unknown mode', mode, disp, res, line_rep)
     # Consume line.
     assert len(data) >= ln, (len(data), ln)
     pixels = data[-ln:]
     data = data[:-ln]
     pixels = pixels[::-1] # Reverse.
-    print(f'line {line_num}: disp {disp} res {res} length {ln}')
+    #print(f'line {line_num}: disp {disp} res {res} length {ln}')
     line_num += 1
 
+    # TODO: deal with line rep
   die
   h = h.split('4020')
   assert len(h[0]) >= 176, len(h[0])
